3577ac2a6dfdff37c45c85c9c7f77284707108a8
Sebastian Koczwara <sebakocz@gmail.com>
2023-01-15 15:27:14 +0100

    add msg when no drafts are available

--- Messages/show_all_drafts_msg.py
+++ Messages/show_all_drafts_msg.py
@@ -7,6 +7,10 @@ from constants import EMBED_COLOR
 async def get_message():
     drafts = await Draft.all()
     embed = discord.Embed(title="All drafts", color=EMBED_COLOR)
+
+    if not drafts:
+        embed.description = "No drafts found. Create one with `/create_draft`!"
+
     for draft in drafts:
         embed.add_field(
             name=draft.name,
